2f92726fda94daa0153094b98d5cc2f9078e8584
MrVeGGi3 <matheusveras500@gmail.com>
2025-01-24 17:11:56 -0300

    Dicionário para chamada de descrição
    Import de CSV e leitura que gera um dicionário afim de facilitar o get da descrição e dos valores para os botões e interação.

---
+++ Scripts/habilites_improvement.gd
@@ -0,0 +1,30 @@
+extends Control
+
+var csv_data = []
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	load_csv("res://Files/csv/Decretos Presidente - Página1.csv")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func load_csv(File_Path : String):
+	var file = FileAccess.open(File_Path, FileAccess.ModeFlags.READ)
+	if file:
+		var headers : Array[String] = file.get_line().strip_edges().split(",")
+		
+		while not file.eof_reached():
+			var line : String = file.get_line().strip_edges()
+			if line != "":
+				var values : Array[String] = line.split(",")
+				var entry : Dictionary = {}
+				
+				for i in range(headers.size()):
+					entry[headers[i]] = values[i]
+					
+				csv_data.append(entry)
+		file.close()
+	else:
+		print("Arquivo CSV não encontrado")
